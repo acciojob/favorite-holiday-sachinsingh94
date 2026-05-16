5644ec83d0fd59d5b45d913142aee9d180e11404
sachinsingh94 <sachinjadaun786@gmail.com>
2026-05-16 23:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// Simple interactivity: highlight list items when clicked
+document.querySelectorAll("li").forEach(item => {
+  item.addEventListener("click", () => {
+    item.style.backgroundColor = "#e0f7fa";
+  });
+});
+
